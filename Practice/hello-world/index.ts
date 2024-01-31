bb56f8873386e93c89b2f850779abbf3bf7d418a
JLRG806 <jlrgonzalez13@gmail.com>
2024-01-31 16:11:57 +0100

    feature: added new advanced types
    - Literal type
    - Nullable type

--- Practice/hello-world/index.ts
+++ Practice/hello-world/index.ts
@@ -162,4 +162,33 @@ const product: Audit & Product = { // In this aproach the object need to have al
     created_at: "13/03/2024",
     modified_at: "13/03/2025"
 
-} 
+}
+
+// Literal Type
+
+// A literal is a more concrete sub-type of a collective type. 
+// What this means is that "Hello World" is a string, but a string is not "Hello World" inside the type system.
+// You can restrict what numbers can be declared, what strings are allowed, or spesify a objec structure with certain limited values.
+type Fibonacci = 0 | 1 | 2 | 3 | 5
+const numOfFibo: Fibonacci = 2
+
+type ValidationSuccess = {
+    isValid: true;
+    reason: null;
+}
+type ValidationFailure = {
+    isValid: false;
+    reason: string;
+}
+type ValidationResult = ValidationSuccess | ValidationFailure;
+
+// Nullable Type
+// This is a way to work with null, undefined types.
+function toNumber(s: string | null | undefined) {
+    if (!s) {
+        return 0
+    }
+    return parseInt(s)
+
+}
+const value = toNumber(null)
