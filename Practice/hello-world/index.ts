565b88fcefd47e5af1cfb7c4756a9379c3cbf85d
jose.rodriguez2 <jose.rodriguez2@ct.uneatlantico.es>
2024-01-31 15:11:04 +0100

    feature: Intro of advanced types.
    - Added Union type
    - Added Intersection type

--- Practice/hello-world/index.ts
+++ Practice/hello-world/index.ts
@@ -1,7 +1,5 @@
 const foo: number = 22
 
-console.log(foo)
-
 let favotiteMeal: string = "meat stew"
 let timeCooking: number = 60
 let isCooked: boolean = true
@@ -115,3 +113,53 @@ function func(msg: string): string {
 function UserError(): never {
     throw new Error("user error")
 }
+
+// ADVANCED TYPES
+
+// Union Type
+
+let score: number | string = 98
+
+score = 'Hello, World!'
+
+
+type Animal = {
+    readonly id: number,
+    status: string
+}
+
+type User = {
+    readonly id: number,
+    name: string
+}
+
+let animal: User | Animal = { id: 1, status: '', name: '' } // in this aproach you can combine 'n' types in one
+
+function sumTwo(n: number | string): number {
+    if (typeof n === 'string') {
+        return parseInt(n) + 2
+    }
+    return n + 2
+}
+
+console.log(sumTwo(3))
+console.log(sumTwo('3'))
+
+
+// Intersection type
+
+type Audit = {
+    created_at: string
+    modified_at: string
+}
+
+type Product = {
+    name: string
+}
+
+const product: Audit & Product = { // In this aproach the object need to have all the properties of the two types.
+    name: "Shampoo",
+    created_at: "13/03/2024",
+    modified_at: "13/03/2025"
+
+} 
